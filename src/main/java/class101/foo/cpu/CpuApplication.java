e1ebf2bfb32d73fcfd6467be0f7765df78f4e481
Sar(g)u <sarguments@gmail.com>
2021-06-23 14:40:20 +0900

    change blank

--- src/main/java/class101/foo/cpu/CpuApplication.java
+++ src/main/java/class101/foo/cpu/CpuApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CpuApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CpuApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(CpuApplication.class, args);
+    }
 
 }
